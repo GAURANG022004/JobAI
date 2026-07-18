d6de3f2eca4a7c8e99e5b9799bec181eadfffe72
Gaurang <gaurangmali11@gmail.com>
2026-07-18 23:34:15 +0530

    Intialize Springboot in Backend

---
+++ backend/JOBAI/src/test/java/com/jobaibackend/JobaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
