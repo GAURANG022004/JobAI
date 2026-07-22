91802a90444054cf12e4fa5a3e5310fe1e4b0844
Gaurang <gaurangmali11@gmail.com>
2026-07-22 23:49:12 +0530

    write pom.xml file

--- backend/JOBAI/src/main/java/com/jobaibackend/JobaiApplication.java
+++ backend/JOBAI/src/main/java/com/jobaibackend/JobaiApplication.java
@@ -6,8 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobaiApplication {
 
+	 private static final Logger logger =
+            LoggerFactory.getLogger(JobAiBackendApplication.class);
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(JobaiApplication.class, args);
+
+		 logger.info("========================================");
+        logger.info("JobAI Backend started successfully.");
+        logger.info("Version : 0.1.0");
+        logger.info("Port    : 8080");
+        logger.info("========================================");
 	}
 
 }
