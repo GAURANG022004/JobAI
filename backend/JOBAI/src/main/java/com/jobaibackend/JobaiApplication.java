f7736fe6aa7855891f741ae95e6581d591fb4e04
Gaurang <gaurangmali11@gmail.com>
2026-07-22 23:52:45 +0530

    test sprinboot application

--- backend/JOBAI/src/main/java/com/jobaibackend/JobaiApplication.java
+++ backend/JOBAI/src/main/java/com/jobaibackend/JobaiApplication.java
@@ -1,5 +1,7 @@
 package com.jobaibackend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,13 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobaiApplication {
 
 	 private static final Logger logger =
-            LoggerFactory.getLogger(JobAiBackendApplication.class);
+            LoggerFactory.getLogger(JobaiApplication.class);
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobaiApplication.class, args);
 
-		 logger.info("========================================");
+		logger.info("========================================");
         logger.info("JobAI Backend started successfully.");
         logger.info("Version : 0.1.0");
         logger.info("Port    : 8080");
